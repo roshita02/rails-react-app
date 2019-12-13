c56eba81ef0987a06ad2eb6f793297d1f86ac65f
roshita02 <roshitashakya07@gmail.com>
2019-12-13 19:48:58 +0545

    Models and seed file created for course section and episodes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+course = Course.create(title: "Hello world!", description: "Create a react on rails app easily");
+section = Section.create(title: 'Chapter 1', course: course);
+episodes = Episode.create([
+    { title: '1. Setting Ruby on rails app', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/pPy0GQJLZUM', section: section },
+    { title: '2. Adding react to existing rails app', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/pPy0GQJLZUM', section: section },
+    { title: '3. Building hello world app', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/pPy0GQJLZUM', section: section },
+    { title: '4. Adding react router dom to app', description: 'lorem ipsum', url: 'https://www.youtube.com/embed/pPy0GQJLZUM', section: section },
+]);
