eab603f90f0feeb8fa1b88346f9711210d3cd695
roshita02 <roshitashakya07@gmail.com>
2019-12-13 21:05:59 +0545

    fetching data from rails

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react';
 import Jumbotron from './Jumbotron';
 import Table from './Table/Table';
+import axios from 'axios';
 
 class Home extends Component{
     constructor(){
         super()
         this.state = {
-            course_modules: [
-                { id: 1, title: '1. Setting Ruby on rails app', description: 'lorem ipsum', active: false },
-                { id: 2, title: '2. Adding react to existing rails app', description: 'lorem ipsum', active: false },
-                { id: 3, title: '3. Building hello world app', description: 'lorem ipsum', active: false },
-                { id: 4, title: '4. Adding react router dom to app', description: 'lorem ipsum', active: false },
-            ]
+            course_modules: []
         }
     }
+
+    componentDidMount(){
+        axios.get('/episodes.json')
+        .then(data => {
+            let res = []
+            data.data.data.map( (data) => {
+                res.push({ id: data.id, title: data.title, description: data.description, active: false})
+                this.setState({course_modules: res})
+            })
+        })
+        .catch(data => {
+            debugger
+        })
+    }
+
     handleVideoChange(item, event){
         event.preventDefault()
         let course_modules = [...this.state.course_modules]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'pages/index'
   root to: 'pages#index'
-  
+  get 'episodes', to: 'pages#episodes'
 end
